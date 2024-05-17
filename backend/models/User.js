9b50a6f3af33680549781282298cb1af314620a0
Darkhan Zhursin <daratechn@gmail.com>
2024-05-17 15:38:27 -0500

    user schema

---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
