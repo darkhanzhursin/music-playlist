892474df943b2197f529834a740048a525620907
Darkhan Zhursin <daratechn@gmail.com>
2024-05-18 19:18:34 -0500

    By default, Mongoose adds an _id property to your schemas.

--- backend/models/User.js
+++ backend/models/User.js
@@ -7,10 +7,6 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  userId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-  },
   password: {
     type: String,
     required: true,
