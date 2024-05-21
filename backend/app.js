12f288610c440592bc8b8609237bbdfa0a1e0219
lipereichert94 <lipereichert94@gmail.com>
2024-05-20 20:25:43 -0500

    hi

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,8 @@ app.use(cors()); // we can configure it later
 // Connect to MongoDB
 connectDB();
 // serve static files
-app.use("/", express.static(path.join(__dirname, "/static")));
+app.use("/music", express.static(path.join(__dirname, "static", "music")));
+//app.use("/music", express.static(path.join(__dirname, "./static")));
 
 // to handle content-type: application/json requests
 app.use(express.json());
@@ -33,6 +34,11 @@ app.use("/users", usersRoute);
 
 app.use(cors); // we can configure it later
 
+// app.use((req, res, next) => {
+//   console.log('4');
+//   res.send('Hi~~~Again');
+// })
+
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
@@ -43,7 +49,8 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
-
+// app.listen(3000, () => console.log(`Server is running on port ${PORT}`));
+console.log(PORT);
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log(process.env.DATABASE_URI);
     await mongoose.connect(process.env.DATABASE_URI);
   } catch (err) {
     console.error(err);
